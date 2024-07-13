1c2742b2838eb84c57d26003d526a7c10e3cf99a
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-07-13 01:15:46 -0400

    Overhaul NDB

--- src/cifparse/cifp_ndb.py
+++ src/cifparse/cifp_ndb.py
@@ -1,67 +1,72 @@
-import cifpfunctions as cf
+from cifp_functions import convert_dms, convert_mag_var
 
 
-class CIFPNDB:
-    def __init__(self, cifpLines: list) -> None:
-        for cifpLine in cifpLines:
-            contRecNo = int(cifpLine[21:22])
-            if contRecNo == 0:
-                self.cont0(cifpLine)
-            if contRecNo == 1:
-                self.cont1(cifpLine)
-
-    def cont0(self, cifpLine: str) -> None:
-        # PAD 1
-        self.area = cifpLine[1:4].strip()
-        self.secCode = cifpLine[4:5].strip()
-        self.subCode = cifpLine[5:6].strip()
-        self.airportId = cifpLine[7:10].strip()
-        self.region = cifpLine[10:12].strip()
-        # PAD 1
-        self.id = cifpLine[13:17].strip()
-        # PAD 2
-        self.subregion = cifpLine[19:21].strip()
-        # self.contRecNo = int(cifpLine[21:22].strip())
-        self.freq = cifpLine[22:27].strip()
+class CIFP_NDB:
+    def __init__(self) -> None:
+        self.area = None
+        self.sec_code = None
+        self.sub_code = None
+        self.airport_id = None
+        self.airport_region = None
+        self.id = None
+        self.region = None
         self.frequency = None
-        self.navClass = cifpLine[27:32].strip()
-        self.latlon = cifpLine[32:51].strip()
+        self.nav_class = None
         self.lat = None
         self.lon = None
+        self.mag_var = None
+        self.datum_code = None
+        self.name = None
+        self.application = None
+        self.notes = None
+        self.record_number = None
+        self.cycle_data = None
+
+    def from_lines(self, cifp_lines: list) -> None:
+        for cifp_line in cifp_lines:
+            cont_rec_no = int(cifp_line[21:22])
+            if cont_rec_no == 0:
+                self._cont0(cifp_line)
+            if cont_rec_no == 1:
+                self._cont1(cifp_line)
+
+    def _cont0(self, cifp_line: str) -> None:
+        # PAD 1
+        self.area = cifp_line[1:4].strip()
+        self.sec_code = cifp_line[4:5].strip()
+        self.sub_code = cifp_line[5:6].strip()
+        self.airport_id = cifp_line[6:10].strip()
+        self.airport_region = cifp_line[10:12].strip()
+        # PAD 1
+        self.id = cifp_line[13:17].strip()
+        # PAD 2
+        self.region = cifp_line[19:21].strip()
+        # self.cont_rec_no = int(cifp_line[21:22].strip())
+        freq = cifp_line[22:27].strip()
+        self.nav_class = cifp_line[27:32].strip()
+        lat_lon = cifp_line[32:51].strip()
         # PAD 23
-        self.variation = cifpLine[74:79].strip()
-        self.magvar = None
+        variation = cifp_line[74:79].strip()
         # PAD 6
         # RESERVED 5
-        self.datumCode = cifpLine[90:93].strip()
-        self.name = cifpLine[93:123].strip()
-        self.recordNo = int(cifpLine[123:128].strip())
-        self.cycleData = cifpLine[128:132].strip()
-        self.translate()
+        self.datum_code = cifp_line[90:93].strip()
+        self.name = cifp_line[93:123].strip()
+        self.record_number = int(cifp_line[123:128].strip())
+        self.cycle_data = cifp_line[128:132].strip()
 
-    def cont1(self, cifpLine: str) -> None:
-        # PAD 22
-        self.application = cifpLine[22:23].strip()
-        self.notes = cifpLine[23:91].strip()
+        if freq != "":
+            self.frequency = int(freq) / 10
 
-    def translate(self) -> None:
-        functions = cf.CIFPFunctions()
-        if self.freq != "":
-            self.frequency = int(self.freq) / 10
-        del self.freq
-
-        if self.latlon != "":
-            coordinates = functions.convertDMS(self.latlon)
+        if lat_lon != "":
+            coordinates = convert_dms(lat_lon)
             self.lat = coordinates.lat
             self.lon = coordinates.lon
-        del self.latlon
 
-        if self.variation != "":
-            magvar = functions.convertMagVar(self.variation)
-            self.magvar = magvar
-        del self.variation
+        if variation != "":
+            mag_var = convert_mag_var(variation)
+            self.mag_var = mag_var
 
-    def toDict(self) -> dict:
-        functions = cf.CIFPFunctions()
-        functions.cleanDict(self.__dict__)
-        return self.__dict__
+    def _cont1(self, cifp_line: str) -> None:
+        # PAD 22
+        self.application = cifp_line[22:23].strip()
+        self.notes = cifp_line[23:91].strip()
