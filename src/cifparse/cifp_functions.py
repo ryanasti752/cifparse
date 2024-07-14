590a7247ea26a8eb3205c6dc38d636e502c5fc33
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-07-13 20:01:37 -0400

    Add translate_rnp Function

--- src/cifparse/cifp_functions.py
+++ src/cifparse/cifp_functions.py
@@ -24,6 +24,15 @@ def convert_mag_var(cifp_mag_var_substring: str) -> float:
     return mag_var_value
 
 
+def translate_rnp(rnp_string: str) -> float:
+    leading_digit_string = rnp_string[0:1]
+    if leading_digit_string == "0":
+        value = int(rnp_string[1:2])
+        exponent = int(rnp_string[2:3])
+        return value * (10**exponent)
+    return int(rnp_string) / 10
+
+
 def chunk(
     line_array: list,
     id_start: int,
